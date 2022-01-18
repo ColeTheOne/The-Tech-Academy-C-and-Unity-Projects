42d148521e990d54601c02ebd3e2ab798de3c435
ColeTheOne <cole.f.gibbs@gmail.com>
2022-01-18 10:49:09 -0800

    Method Assignment 3

---
+++ Method Assignment 3/Method Assignment 3/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment_3
+{
+    public class Math
+    {
+        public int doMath (int num1, int num2 = 0)
+        {
+            int result = num1 + num2;
+            return result;
+        }
+    }
+}
---
+++ Method Assignment 3/Method Assignment 3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+
+            Console.WriteLine("Please enter a number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter another number (you may leave this blank if you wish):");
+            string Number = Console.ReadLine();
+
+            try
+            {
+                int num2 = Convert.ToInt32(Number);
+                int sum = math.doMath(num1, num2);
+                Console.WriteLine("Your two numbers added together equals: {0}", sum);
+            }
+            catch (FormatException)
+            {
+                int sum = math.doMath(num1);
+                Console.WriteLine("Your two numbers added together equals: {0}", sum);
+            }
+
+          
+            Console.ReadLine();
+        }
+    }
+}

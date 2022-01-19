6d40fddc1a94f8e3614b0a999ee447bcbfb27b40
ColeTheOne <cole.f.gibbs@gmail.com>
2022-01-18 20:56:32 -0800

    Abstract plus interface Assignment

---
+++ Abstract Assignment/Abstract Assignment/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Assignment
+{
+    public class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Name: {0} {1}", firstName, lastName);
+        }
+        public void Quit()
+        {
+            Console.WriteLine("You have quit the company");
+        }
+    }
+}
---
+++ Abstract Assignment/Abstract Assignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Employee employee = new Employee();
+            //employee.firstName = "Example";
+            //employee.lastName = "Student";
+            //employee.SayName();
+
+            IQuittable quittable = new Employee();
+
+            Console.WriteLine("Do you wish to quit the company? Type \"true\" or \"false\"");
+            bool quitting = Convert.ToBoolean(Console.ReadLine());
+
+            if (quitting)
+            {
+                quittable.Quit();
+            }
+            else 
+            { 
+                Console.WriteLine("Thank You"); 
+            }
+            Console.ReadLine();
+        }
+    }
+}

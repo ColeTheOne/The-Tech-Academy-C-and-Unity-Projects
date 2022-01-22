277a75fb627a2f650306b75033c65a184d29e0e6
ColeTheOne <cole.f.gibbs@gmail.com>
2022-01-21 21:16:19 -0800

    Parsing Enums Assignment

---
+++ Parsing Enums Assignment/Parsing Enums Assignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsing_Enums_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter which day of the week it is.");
+                string day = Console.ReadLine();
+                DayoftheWeek dayoftheWeek = (DayoftheWeek)Enum.Parse(typeof(DayoftheWeek), day);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            Console.ReadLine();
+        }
+        public enum DayoftheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}

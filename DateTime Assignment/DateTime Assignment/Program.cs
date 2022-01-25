87c3022f82b4da5802db5fe31459eef4560fd09f
ColeTheOne <cole.f.gibbs@gmail.com>
2022-01-25 14:41:34 -0800

    DateTime Assignment

---
+++ DateTime Assignment/DateTime Assignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime time = DateTime.Now;
+            Console.WriteLine("It is now {0}", time);
+            Console.WriteLine("Please enter a number.");
+            int num = Convert.ToInt32(Console.ReadLine());
+            time = time.AddHours(num);
+            Console.WriteLine("In {0} hours it will be {1}", num, time);
+            Console.Read();
+        }
+    }
+}
